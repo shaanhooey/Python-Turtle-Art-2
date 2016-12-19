f40958e03f16c933239f1204d08bc3e7542dd16e
shaanhooey <shaan.hooey@ucc.on.ca>
2016-12-19 15:48:00 -0500

    Initial Commit

---
+++ House.py
@@ -0,0 +1,103 @@
+import turtle
+import random
+
+shaan = turtle.Turtle()
+
+#shaan.color(50,50,50)
+
+#shaan.begin_fill()
+for i in range (2):
+	shaan.forward(200)
+	shaan.right(90)
+	shaan.forward(250)
+	shaan.right(90)
+
+shaan.left(60)
+
+for i in range(4):
+	shaan.forward(200)
+	shaan.right(120)
+
+shaan.forward(100)
+shaan.left(150)
+shaan.forward(100)
+shaan.right(90)
+shaan.forward(20)
+shaan.right(90)
+shaan.forward(135)
+shaan.left(30)
+shaan.forward(60)
+
+shaan.right(120)
+shaan.forward(32.5)
+shaan.pu()
+shaan.left(90)
+shaan.forward(60)
+shaan.pd()
+
+
+for i in range(4):
+	shaan.forward(25)
+	shaan.right(90)
+
+shaan.forward(25)
+shaan.right(90)
+shaan.forward(25)
+shaan.pu()
+shaan.forward(30)
+shaan.pd()
+
+
+for i in range(4):
+	shaan.forward(25)
+	shaan.right(90)
+
+shaan.forward(25)
+shaan.pu()
+shaan.forward(30)
+shaan.pd()
+
+for i in range(4):
+	shaan.forward(25)
+	shaan.right(90)
+
+shaan.forward(25)
+shaan.pu()
+shaan.forward(32.5)
+shaan.pd()
+
+
+shaan.left(90)
+shaan.forward(165)
+shaan.left(90)
+shaan.forward(100)
+shaan.left(90)
+shaan.forward(100)
+shaan.right(90)
+shaan.forward(50)
+shaan.right(90)
+shaan.forward(100)
+shaan.right(180)
+shaan.forward(75)
+shaan.pu()
+shaan.left(90)
+shaan.forward(20)
+shaan.pd()
+shaan.begin_fill()
+shaan.circle(5,360)
+shaan.end_fill()
+shaan.pu()
+shaan.forward(1000)
+
+turtle.done()
+
+
+
+
+
+
+
+
+
+
+#shaan.end_fill()
